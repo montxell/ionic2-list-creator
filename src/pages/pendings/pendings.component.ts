69fef264e8332a23c7160109d72cfc60f7fb0385
Montse Ariso <montxelltech@gmail.com>
2017-05-28 01:15:46 +0200

    Create 'pendings' and 'done' components, modify tabs and app.module

---
+++ src/pages/pendings/pendings.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pendings',
+  templateUrl: 'pendings.component.html',
+})
+export class PendingsComponent implements OnInit {
+  constructor() {  }
+
+  ngOnInit() {}
+}
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { PendingsComponent } from '../pendings/pendings.component';
+import { DoneComponent } from '../done/done.component';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = PendingsComponent;
+  tab2Root = DoneComponent;
 
   constructor() {
 

6162e79c1f450d877d9df5ce14c2218192c735b2
Montse Ariso <montxelltech@gmail.com>
2017-05-29 09:53:11 +0200

    Add method 'saveList' with an alert

--- src/pages/add/add.component.ts
+++ src/pages/add/add.component.ts
@@ -1,7 +1,11 @@
 
 import { Component, OnInit } from '@angular/core';
+import { AlertController, NavController } from 'ionic-angular';
 
 import { List, ListItem } from '../../app/classes/index';
+
+import { ListService } from '../../app/services/list.service';
+
 
 @Component({
   selector: 'app-add',
@@ -9,14 +13,20 @@ import { List, ListItem } from '../../app/classes/index';
 })
 export class AddComponent implements OnInit {
 
-  listName:string;
-  itemName:string="";
+  listName:string = "";
+  itemName:string = "";
 
   items:ListItem[] = [];
 
-  constructor() {  }
+  constructor(
+    public alertCtrl: AlertController,
+    public navCtrl: NavController,
+    public _listService: ListService
+  ) {  }
+
 
   ngOnInit() {}
+
 
   addItem() {
     if (this.itemName.length == 0) {
@@ -31,9 +41,32 @@ export class AddComponent implements OnInit {
 
   }
 
+
   deleteItem( index:number ) {
     this.items.splice(index, 1);
   }
 
 
-}
+  saveList() {
+    if (this.listName.length == 0) {
+
+      let alert = this.alertCtrl.create({
+            title: 'Nombre de la lista (List name)',
+            subTitle: 'El nombre de la lista es necesario (Name list required)',
+            buttons: ['OK']
+          });
+          alert.present();
+          return;
+      }
+
+      let list = new List( this.listName );
+      list.items = this.items;
+
+      this._listService.lists.push( list );
+      this.navCtrl.pop();
+
+    }
+
+
+
+  }

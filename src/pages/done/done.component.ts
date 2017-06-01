bf64ec0a8d9d1084140622d10f8db07d2be6ae14
Montse Ariso <montxelltech@gmail.com>
2017-06-01 02:39:00 +0200

    Add code to show 'done' lists

--- src/pages/done/done.component.ts
+++ src/pages/done/done.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ListService } from '../../app/services/list.service';
+
+import { NavController } from 'ionic-angular';
+import { DetailComponent } from '../detail/detail.component';
+
+
+
 @Component({
   selector: 'app-done',
   templateUrl: 'done.component.html',
 })
 export class DoneComponent implements OnInit {
-  constructor() {  }
+  constructor( private _listService: ListService,
+               private navCtrl: NavController) {  }
 
   ngOnInit() {}
+
+
+  seeDetail( list, index ) {
+    this.navCtrl.push( DetailComponent, { list, index } );
+  }
+
 }
